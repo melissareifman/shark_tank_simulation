8439ed0e9fd62eaa5ae32d188814045e149b5ced
Melissa Reifman <mbr87@cornell.edu>
2024-03-19 15:05:37 -0400

    working on parsing

--- backend/helpers/parse.py
+++ backend/helpers/parse.py
@@ -1,3 +1,4 @@
+"""
 import pandas
 
 import json
@@ -8,3 +9,25 @@ json_str = excel_data_df.to_json()
 
 with open('pitchesdeals.json', 'w') as f:
     json.dump(json_str, f)
+"""
+
+import csv
+import json
+ 
+def make_json(csvFilePath, jsonFilePath):
+    data = {}
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+        for rows in csvReader:
+            print(rows)
+            key = rows['Pitched_Business_Identifier']
+            data[key] = rows
+ 
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
+
+csvFilePath = './parse.py'
+jsonFilePath = './pitchesdeals.json'
+ 
+make_json(csvFilePath, jsonFilePath)
