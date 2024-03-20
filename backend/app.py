ed04ddfd73cb5a783119e6af79b324477c0d7b18
Reese Thompson <rqt3@cornell.edu>
2024-03-20 14:56:33 -0400

    updated home screen image change search query method

--- backend/app.py
+++ backend/app.py
@@ -22,10 +22,19 @@ with open(json_file_path, 'r') as file:
 app = Flask(__name__)
 CORS(app)
 
-# Sample search using json with pandas
+
 def json_search(query):
-    matches = pitches_df[pitches_df['Pitched_Business_Desc'].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['Pitched_Business_Identifier', 'Pitched_Business_Desc', 'Deal_Status', 'Deal_Shark']]
+
+    query_words = set(query.lower().split())
+    query_word_count = len(query_words)
+    # check if any query word is in the pitch description
+    def is_match(desc):
+        desc_words = set(desc.lower().split())
+        return not query_words.isdisjoint(desc_words)  # true if any word in common
+
+    matches = pitches_df[pitches_df['Pitched_Business_Desc'].apply(is_match)]
+    # matches = pitches_df[pitches_df['Pitched_Business_Desc'].str.lower().str.contains(query.lower())]
+    matches_filtered = matches[['Pitched_Business_Identifier', 'Pitched_Business_Desc', 'Deal_Status', 'Deal_Shark', 'US_Viewership']]
     matches_filtered_json = matches_filtered.to_json(orient='records')
     return matches_filtered_json
 
