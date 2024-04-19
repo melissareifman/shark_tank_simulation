7e014dcd41dc9a4d64a64c926edc792b3113f39e
Melissa Reifman <mbr87@cornell.edu>
2024-04-19 12:44:16 -0400

    Attempted to implement Rocchio

--- backend/app.py
+++ backend/app.py
@@ -27,14 +27,22 @@ with open(json_file_path, 'r') as file:
 # end of preprocess
 app = Flask(__name__)
 CORS(app)
+feedback_data = {}
 
 
-def json_search(query):
+def json_search(query, feedback=None):
 
     query_words = set(query.lower().split())
     query_word_count = len(query_words)
     ### WE SHOULD REMOVE STOP WORDS ###
     tfidf_query = compute_query_vector(query, consts.idf_dict) 
+
+    #update query vector based on rocchio feedback
+    if feedback:
+        relevant = [compute_document_vector(doc_id, consts.inverted_index, consts.idf_dict, consts.n_docs) for doc_id, rel in feedback if rel]
+        irrelevant = [compute_document_vector(doc_id, consts.inverted_index, consts.idf_dict, consts.n_docs) for doc_id, rel in feedback if not rel]
+        tfidf_query = rocchio(tfidf_query, relevant, irrelevant)
+
     similarities = []
     # This is a numpy array
     tfidf_matrix = create_tfidf_matrix(pitches_df.iterrows())
@@ -113,6 +121,37 @@ def compute_document_vector(doc_id, inverted_index, idf_dict, n_docs):
                 tfidf_vector[list(idf_dict.keys()).index(term)] = tf * idf_value
     return tfidf_vector
 
+def rocchio(query, relevant, irrelevant, a=.3, b=.3, c=.8, clip = True):
+    """Returns a vector representing the modified query vector. 
+    
+    Note: 
+        If the `clip` parameter is set to True, the resulting vector should have 
+        no negatve weights in it!
+    """
+    q0 = query
+    relevant_docs = np.array(relevant)
+    irrelevant_docs = np.array(irrelevant)
+    
+    term1 = a * q0
+    if len(relevant) == 0:
+        term2 = np.zeros(len(q0))
+    else:
+        term2 = b * np.mean(relevant_docs, axis=0)
+    if len(irrelevant) == 0:
+        term3 = np.zeros(len(q0))
+    else:
+        term3 = c * np.mean(irrelevant_docs, axis=0)
+        
+    res = term1 + term2 - term3
+    
+    if clip:
+        for i in range(len(res)):
+            if res[i] < 0:
+                res[i] = 0
+            
+    return res
+
+
 def cosine_similarity(tfidf_doc, tfidf_query, doc_norm):
     """
     Calculate the cosine similarity between a document and the query.
@@ -134,9 +173,11 @@ def pitches_search():
     if not text:
         # Return an error message or an empty JSON if no query parameter is provided
         return {"error": "No query provided"}, 400  # 400 Bad Request
+    
+    feedback = feedback_data.get(text,[])
 
     # Call the search function with the user input
-    result = json_search(text)
+    result = json_search(text, feedback)
 
     # Check if the search result is empty
     if not result:
@@ -144,6 +185,16 @@ def pitches_search():
         return {"message": "No matches found"}, 404  # 404 Not Found
 
     return result
+
+@app.route("/feedback", methods=["POST"])
+def receive_feedback():
+    feedback = request.get_json()
+    query = feedback["query"]
+    documents = feedback["documents"]
+
+    feedback_data[query] = documents
+
+    return {"message" : "Feedback received successfully"}, 200
 
 
 if 'DB_NAME' not in os.environ:
